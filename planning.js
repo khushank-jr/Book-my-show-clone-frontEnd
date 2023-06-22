41f99263d7a48202d923d058bbd9be7636492b31
Khushank Jr <91781337+khushank-itmur@users.noreply.github.com>
2023-06-22 14:02:58 +0530

    first commit

---
+++ planning.js
@@ -0,0 +1,26 @@
+//BOOK MY SHOW PLANNING
+ /*
+ We need a reusable Carousel
+1) Hero carousel 
+2)Common Carousel
+3)Cast Carousle (small round)
+---------------------------------------------------------------
+//PAGES
+
+Home page
+Movie page
+categories page
+auth pages
+razorpay payment gateway
+
+---------------------------------------------------------------
+//Plan how we are building it?
+
+LAyouts
+Routes
+Pages
+Components 
+
+---------------------------------------------------------------
+
+ */
